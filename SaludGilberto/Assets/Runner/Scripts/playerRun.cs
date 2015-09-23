88e0e56dfcca06ae1d36012d04a3cc0699faf066
EdgarB <edgar_daniel_1996@hotmail.com>
2015-09-23 16:03:35 -0500

    Scene updated with the sprites and smooth movement added

--- SaludGilberto/Assets/Runner/Scripts/playerRun.cs
+++ SaludGilberto/Assets/Runner/Scripts/playerRun.cs
@@ -3,49 +3,104 @@ using System.Collections;
 
 public class playerRun : MonoBehaviour {
 
-    public float speed;
-    private bool down, middle, up;
+    public float speed, percentage;
+    private bool down, middle, up, bMoving, bDir; 
+    private Vector3 vPosGoal;
+    
 
     void Start()
     {
         down = true;
         middle = false;
         up = false;
+        bMoving = false; //True if its in the air
+        vPosGoal = transform.position;
+        bDir = true; //If true is going Up , if false is going down
     }
 
     // Update is called once per frame
     void Update()
     {
+        //DEFINE LINE
         //this.transform.Translate(Vector3.right * speed * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !bMoving)
         {
             if (down)
             {
                 down = up = false;
                 middle = true;
-                this.transform.position += Vector3.up*1.7f;
+                vPosGoal = transform.position + Vector3.up;
+                bDir = true;
+                //this.transform.position += Vector3.up;
             } 
             else if (middle)
             {
                 down = middle = false;
                 up = true;
-                this.transform.position += Vector3.up*1.7f;
+                vPosGoal = transform.position + Vector3.up;
+                bDir = true;
+                //this.transform.position += Vector3.up;
             }
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && !bMoving)
         {
             if (up)
             {
                 up = down = false;
                 middle = true;
-                this.transform.position -= Vector3.up*1.7f;
+                vPosGoal = transform.position - Vector3.up;
+                bDir = false;
+                //this.transform.position -= Vector3.up;
             }
             else if (middle)
             {
                 middle = up = false;
                 down = true;
-                this.transform.position -= Vector3.up*1.7f;
+                vPosGoal = transform.position - Vector3.up;
+                bDir = false;
+                //this.transform.position -= Vector3.up;
             }
         }
+
+
+        //MOVE TO LINE
+        if(up)
+        {
+            transform.position = vMoveFromTo(transform.position, vPosGoal, speed, percentage);
+            
+        }else if(middle)
+        {            
+            transform.position = vMoveFromTo(transform.position, vPosGoal, speed, percentage);
+        }
+        else if(down)
+        {            
+            transform.position = vMoveFromTo(transform.position, vPosGoal, speed, percentage);
+        }
+
+        //Check if it is at the goal position
+        if (transform.position == vPosGoal)
+        {
+            bMoving = false;
+        }
+        else
+        {
+            bMoving = true;
+        }
+
     }
+
+    Vector3 vMoveFromTo(Vector3 v3PosInit, Vector3 v3PosFin, float fTime, float fPerc)
+    {
+        Vector3 vReturn;
+        //Checar porcentaje
+        if (bDir && v3PosInit.y >= (v3PosFin.y * fPerc) / 100)
+        {
+            vReturn = v3PosFin;
+        }else if(!bDir && v3PosInit.y <= (v3PosFin.y * fPerc) / 100)
+            vReturn = v3PosFin;
+        else
+            vReturn = Vector3.Lerp(v3PosInit, v3PosFin, fTime); 
+        return vReturn;
+    }
+
 }
--- SaludGilberto/Assets/Runner/Scripts/spawnObjects.cs
+++ SaludGilberto/Assets/Runner/Scripts/spawnObjects.cs
@@ -1,52 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class spawnObjects : MonoBehaviour {
+public class spawnObjects : MonoBehaviour
+{
 
     public Transform pos;
     public GameObject prefab;
 
-	// Use this for initialization
-	void Start () {
+    public Sprite[] sprites;
+    private SpriteRenderer spriteR;
+
+    // Use this for initialization
+    void Start()
+    {
+        spriteR = prefab.GetComponent<SpriteRenderer>();
         InvokeRepeating("SpawnObjects", 1, 1f);
-	}
-	
-	void SpawnObjects()
+    }
+
+    void SpawnObjects()
     {
         int num = (int)Mathf.Floor(Random.Range(1, 6));
         switch (num)
         {
             case 1:
                 {
+                    int num2 = (int)Mathf.Floor(Random.Range(0, 15));
+                    spriteR.sprite = sprites[num2 % (sprites.Length - 1)];
                     Instantiate(prefab, pos.position, pos.rotation);
                     break;
                 }
             case 2:
                 {
-                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y + 1.7f), pos.rotation);
+                    int num2 = (int)Mathf.Floor(Random.Range(0, 15));
+                    spriteR.sprite = sprites[num2 % (sprites.Length - 1)];
+                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y + 1f), pos.rotation);
                     break;
                 }
             case 3:
                 {
-                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y - 1.7f), pos.rotation);
+                    int num2 = (int)Mathf.Floor(Random.Range(0, 15));
+                    spriteR.sprite = sprites[num2 % (sprites.Length - 1)];
+                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y - 1f), pos.rotation);
                     break;
                 }
             case 4:
                 {
-                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y + 1.7f), pos.rotation);
-                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y - 1.7f), pos.rotation);
+                    int num2 = (int)Mathf.Floor(Random.Range(0, 15));
+                    int num3 = (int)Mathf.Floor(Random.Range(0, 15));
+                    spriteR.sprite = sprites[num2 % (sprites.Length - 1)];
+                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y + 1f), pos.rotation);
+                    if (num3 > 4)
+                        spriteR.sprite = sprites[4];
+                    else
+                        spriteR.sprite = sprites[num3 % (sprites.Length - 1)];
+                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y - 1f), pos.rotation);
                     break;
                 }
             case 5:
                 {
+                    int num2 = (int)Mathf.Floor(Random.Range(0, 15));
+                    int num3 = (int)Mathf.Floor(Random.Range(0, 15));
+                    spriteR.sprite = sprites[num2 % (sprites.Length - 1)];
                     Instantiate(prefab, pos.position, pos.rotation);
-                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y - 1.7f), pos.rotation);
+                    if (num3 > 4)
+                        spriteR.sprite = sprites[4];
+                    else
+                        spriteR.sprite = sprites[num3 % (sprites.Length - 1)];
+                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y - 1f), pos.rotation);
                     break;
                 }
             case 6:
                 {
+                    int num2 = (int)Mathf.Floor(Random.Range(0, 15));
+                    int num3 = (int)Mathf.Floor(Random.Range(0, 15));
+                    spriteR.sprite = sprites[num2 % (sprites.Length - 1)];
                     Instantiate(prefab, pos.position, pos.rotation);
-                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y + 1.7f), pos.rotation);
+                    if (num3 > 4)
+                        spriteR.sprite = sprites[4];
+                    else
+                        spriteR.sprite = sprites[num3 % (sprites.Length - 1)];
+                    Instantiate(prefab, new Vector3(pos.position.x, pos.position.y + 1f), pos.rotation);
                     break;
                 }
             default:
